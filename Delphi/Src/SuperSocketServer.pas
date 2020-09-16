2dcb525db0fdbc093c4383f29a1a8d9e63f0717b
Ryu <ryujt658@hanmail.net>
2020-09-16 13:34:09 +0900

    Update SuperSocketServer.pas

--- Delphi/Src/SuperSocketServer.pas
+++ Delphi/Src/SuperSocketServer.pas
@@ -714,7 +714,7 @@ begin
   if AConnection.FSocket = INVALID_SOCKET then Exit;
 
   pData := FIODataPool.Get;
-  PData^.wsaBuffer.buf := FMemoryRecylce.Get;
+  PData^.wsaBuffer.buf := FMemoryRecylce.Get(PACKET_SIZE);
   pData^.wsaBuffer.len := PACKET_SIZE;
   pData^.Status := ioRecv;
   pData^.Connection := AConnection;
